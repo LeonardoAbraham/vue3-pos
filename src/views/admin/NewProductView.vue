ebb4b13babcaca0d819448008e7791550b6d3704
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-02-04 23:17:27 -0600

    Instalando y configurando FormKit

--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -11,6 +11,15 @@
         </Link>
 
         <h1 class="text-4xl font-black my-10">Nuevo Producto</h1>
+
+        <FormKit
+            type="form"
+        >
+            <FormKit 
+                type="text"
+            />
+        </FormKit>
+
     </div>
 </template>
 
