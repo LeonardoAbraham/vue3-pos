1a021a1f205e731fab46dd9ad99fd885ee0a5c88
Leonardo Alonzo <leonardo.a@cliker.mx>
2024-02-06 10:43:56 -0600

    CSS a los inputs y al boton de submit

--- formkit.config.js
+++ formkit.config.js
@@ -5,19 +5,24 @@ const config = {
             global: {
                 label: 'block mb-1 font-bold text-lg',
                 message: 'text-red-500',
-                wrapper: 'space-y-2 mb-3'
+                wrapper: 'space-y-2 mb-3',
+                input: 'w-full p-3 border border-gray-300 rounded text-gray-700 placeholder-gray-400'
             },
             text: {
                 //message:'$reset text-green-500'
             },
             file: {
-
+                noFiles:'block my-2',
+                fileItem: 'hidden'
             },
             select: {
 
             },
             number: {
-                message: 'text-red-500'
+
+            },
+            submit: {
+                input:'$reset bg-green-400 hover:bg-green-500 w-full p-2 font-bold uppercase'
             }
         })
     }
