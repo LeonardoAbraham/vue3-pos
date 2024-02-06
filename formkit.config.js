fd74e51f911a0dcc94a58ac7d25439a52477c359
Leonardo Alonzo <leonardo.a@cliker.mx>
2024-02-06 10:32:38 -0600

    Como funciona $reset y añadiendo CSS al wrapper

--- formkit.config.js
+++ formkit.config.js
@@ -2,8 +2,19 @@ import { generateClasses } from '@formkit/themes'
 const config = {
     config: {
         classes: generateClasses({
+            global: {
+                label: 'block mb-1 font-bold text-lg',
+                message: 'text-red-500',
+                wrapper: 'space-y-2 mb-3'
+            },
             text: {
-                message: 'text-red-500'
+                //message:'$reset text-green-500'
+            },
+            file: {
+
+            },
+            select: {
+
             },
             number: {
                 message: 'text-red-500'
