d00abaed74c9655d379979c6afa7933673c1043e
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2025-01-04 23:46:00 -0600

    Subir imágenes a Firebase Storage

--- src/composables/useImage.js
+++ src/composables/useImage.js
@@ -1,7 +1,31 @@
+import { useFirebaseStorage } from 'vuefire'
+import { ref as storageRef, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { uid } from 'uid';
+
 export default function useImage() {
 
+    const storage = useFirebaseStorage();
+
     const onFileChange = e => {
-        console.log(e.target.files[0])
+        const file = e.target.files[0];
+        const filename = uid() + '.jpg'
+        const sRef = storageRef(storage, '/products/' + filename)
+
+        //Sube el archivo
+        const uploadTask = uploadBytesResumable(sRef, file)
+
+        uploadTask.on('state_changed', 
+            () => {},
+            (error) => console.log(error),
+            () => {
+                //Upload is complete
+                console.log(uploadTask.snapshot.ref)
+                getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL) => {
+                        console.log(downloadURL)
+                    })
+            }
+        )
     }
 
     return {
