adad8c1eab17006be7783a78e0d925e2dc104d29
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2025-01-07 14:20:56 -0600

    Mostrando los Productos

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { formatCurrency } from '@/helpers';
+
     defineProps({
         product:{
             type: Object
@@ -7,7 +9,20 @@
 </script>
 
 <template>
-    <div>{{ product.name }}</div>
+    <li
+        class="flex items-center space-x-6 border border-gray-200 p-6 bg-white shadow"
+    >
+        <img 
+            :src="product.image" 
+            :alt="product.name"
+            class="h-24 w-24"
+        >
+        <div class="space-y-2">
+            <h3 class="text-gray-900">{{ product.name }}</h3>
+            <p class="font-extrabold">{{ formatCurrency(product.price) }}</p>
+            <p>{{ product.availability }}</p>
+        </div>
+    </li>
 </template>
 
 <style scoped>
